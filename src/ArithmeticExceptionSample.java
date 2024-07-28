78a9403617d70e62e8b79ee71b4bf9f0115ce923
KEISHIN-kira <ykira@lycorp.co.jp>
2024-07-28 22:59:42 +0900

    Javaプログラミング入門学習終了

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,11 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int a = 10/0;
+        System.out.println(a);
+
+    }
+
+}
